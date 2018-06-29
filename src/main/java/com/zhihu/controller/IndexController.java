d891c1f04e3418ea6771b4fc4543114022550bc4
harryzhou1103 <yzhou108@dons.usfca.edu>
2018-06-29 17:15:45 -0400

    Downgrade the Spring Boot version to 1.3.6

--- src/main/java/com/zhihu/controller/IndexController.java
+++ src/main/java/com/zhihu/controller/IndexController.java
@@ -3,14 +3,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class IndexController {
-
-    @RequestMapping(path = {"/vm"}, method = {RequestMethod.GET})
-    public String template(Model model) {
-        model.addAttribute("value1", "vvvvv1");
-        return "home";
-    }
 
     @RequestMapping(path = {"/index"}, method = {RequestMethod.GET})
     @ResponseBody
@@ -26,6 +23,13 @@ public class IndexController {
                           @RequestParam(value = "type", defaultValue = "1") int type,
                           @RequestParam(value = "page", required = false) String key) {
         return String.format("Profile Page of %s / %d, t:%d k: %s", groupId, userId, type, key);
+    }
+
+    @RequestMapping(path = {"/vm"}, method = {RequestMethod.GET})
+    public String template(Model model) {
+        List<String> colors = Arrays.asList(new String[] {"RED", "GREEN"});
+        model.addAttribute("colors", colors);
+        return "home";
     }
 
 
