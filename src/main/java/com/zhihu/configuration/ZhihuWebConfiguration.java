b60b5bd633518b9a7f3d4936d5775f9e70943347
harrzhou1103 <yzhou108@dons.usfca.edu>
2018-07-05 15:46:01 -0400

    Implemented the QuestionService. Now you can add more questions. Implemented the SensitiveService, it will filter the specified sensitive words(like dirty words)

--- src/main/java/com/zhihu/configuration/ZhihuWebConfiguration.java
+++ src/main/java/com/zhihu/configuration/ZhihuWebConfiguration.java
@@ -3,9 +3,11 @@ package com.zhihu.configuration;
 import com.zhihu.interceptor.LoginRequiredInterceptor;
 import com.zhihu.interceptor.PassportInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Component
 public class ZhihuWebConfiguration extends WebMvcConfigurerAdapter{
     @Autowired
     PassportInterceptor passportInterceptor;
--- src/main/java/com/zhihu/dao/QuestionDAO.java
+++ src/main/java/com/zhihu/dao/QuestionDAO.java
@@ -8,24 +8,20 @@ import java.util.List;
 
 @Mapper
 public interface QuestionDAO {
-    String TABLE_NAME = "question";
-    String INSERT_FILEDS = "title, content, create_date, user_id, comment_count";
-    String SELECT_FILEDS = "id, " + INSERT_FILEDS;
+    String TABLE_NAME = " question ";
+    String INSERT_FIELDS = " title, content, created_date, user_id, comment_count ";
+    String SELECT_FIELDS = " id, " + INSERT_FIELDS;
 
-
-    @Insert({"insert into", TABLE_NAME, "("
-            , INSERT_FILEDS, ") values(#{title}, #{content}, #{createdDate}, #{userId}, #{commentCount})"})
+    @Insert({"insert into ", TABLE_NAME, "(", INSERT_FIELDS,
+            ") values (#{title},#{content},#{createdDate},#{userId},#{commentCount})"})
     int addQuestion(Question question);
 
-    List<Question> selectLatestQuestions(@Param("userId") int userId,
-                                         @Param("offset") int offset,
+    List<Question> selectLatestQuestions(@Param("userId") int userId, @Param("offset") int offset,
                                          @Param("limit") int limit);
-    @Select({"select ", SELECT_FILEDS, " from ", TABLE_NAME, " where id=#{id}"})
+
+    @Select({"select ", SELECT_FIELDS, " from ", TABLE_NAME, " where id=#{id}"})
     Question getById(int id);
 
-    @Update({"update", TABLE_NAME, " set password=#{password} where id=#{id}"})
-    void updatePassword(User user);
-
-    @Delete({"delete from ", TABLE_NAME, " where id=#{id}"})
-    void deleteById(int id);
+    @Update({"update ", TABLE_NAME, " set comment_count = #{commentCount} where id=#{id}"})
+    int updateCommentCount(@Param("id") int id, @Param("commentCount") int commentCount);
 }
